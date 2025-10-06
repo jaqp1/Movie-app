f15f1bd648b55686eb93fd3ab0475c4d110e836d
jaqp1 <kuba.wenek@wp.pl>
2025-10-06 22:52:50 +0200

    Initial commit

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const profile = () => {
+  return (
+    <View>
+      <Text>profile</Text>
+    </View>
+  )
+}
+
+export default profile
